bd42f322d9e11f4cca3fd79390fc8ac575f841f0
gautam-ghai <gautamghai99@gmail.com>
2020-11-17 22:17:52 -0800

    Static React Site

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -43,7 +43,7 @@ const About = () => {
           onLoadSuccess={onDocumentLoadSuccess}
           className="document"
         >
-          <Page pageNumber={pageNumber} height="590" width="430"/>
+          <Page pageNumber={pageNumber} height="570" width="415"/>
         </Document>
         <div>
           <p>
